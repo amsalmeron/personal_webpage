ad6277f1cf7828aa65048818c8ba8970e8641287
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-01-02 19:02:33 +0100

    Add: button to return home to all pages

--- spec/features/portfolio/index_spec.rb
+++ spec/features/portfolio/index_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe "portfolio page" do
     it 'has my a description of a project in portfolio: Dude, Hows My City?' do
         visit portfolio_index_path
         expect(current_path).to  eq('/portfolio')
-        expect(page).to have_content("Dude, How's My City?") 
+        expect(page).to have_content("Dude, How's My City?")
+
+        click_button 'Home'
+        expect(current_path).to eq(root_path)
     end
 end
