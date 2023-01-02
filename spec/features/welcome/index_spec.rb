8dd36fa9b0eaa4029f18fecebe723fa926069a71
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-01-02 18:38:58 +0100

    Add: root path, remove coverage from git, tests pass

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe "welcome page" do
     
     it 'has my name, introduction and home button' do
-        visit '/'
+        visit root_path
         expect(page).to have_content('Antonio')
         expect(page).to have_content('Software Developer')
 
         click_button 'Home'
-        expect(current_path).to eq('/')
+        expect(current_path).to eq(root_path)
     end
 end
