b7a8bba85a05049f06cf2178af905b84efdef531
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-01-02 18:48:15 +0100

    Fix: test coverage (all tests apss)

--- spec/features/portfolio/index_spec.rb
+++ spec/features/portfolio/index_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe "portfolio page" do
     
     it 'has my a description of a project in portfolio: Dude, Hows My City?' do
-        visit portfolio_path
-
+        visit portfolio_index_path
+        expect(current_path).to  eq('/portfolio')
         expect(page).to have_content("Dude, How's My City?") 
     end
 end
